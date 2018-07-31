1ce0fc8e3e8dd0c1bb38138b67fd3e972b4b74fc
Elchyn <mr.chelyabiev14@gmail.com>
2018-07-31 17:16:02 +0300

    Functional admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    # what to display in admin site
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    # filter options
+    list_filter = ('status', 'created', 'publish', 'author')
+    # searchable fields
+    search_fields = ('title', 'body')
+    # auto-filling slug with title
+    prepopulated_fields = {'slug': ('title',)}
+    # used for lookup widget not drop-down
+    raw_id_fields = ('author',)
+    # hierarchy under search
+    date_hierarchy = 'publish'
+    # default ordering
+    ordering = ('status', 'publish')
