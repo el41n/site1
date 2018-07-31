628798167c712fe1d21a0b09d17d2c7bad788d59
Elchyn <mr.chelyabiev14@gmail.com>
2018-07-31 17:41:50 +0300

    Custom manager for puslished posts

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,12 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -22,8 +28,12 @@ class Post(models.Model):
                               choices=STATUS_CHOICES,
                               default='draft')
 
+    objects = models.Manager()
+    published = PublishedManager()
+
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
